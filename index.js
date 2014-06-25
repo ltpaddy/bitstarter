c09323d14fa5cb500534890ad3164ab8eecca2a7
ltpaddy <ltpaddy5@gmail.com>
2014-06-25 20:34:29 +0000

    Added semicolons

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var indexContents = fs.readFileSync('index.html').toString()
+var indexContents = fs.readFileSync('index.html').toString();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(indexContents)
-})
+  response.send(indexContents);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
